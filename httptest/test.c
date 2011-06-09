a649dc353e091fb98c7b0a27214e6aed8cca7635
li hao <lihao814386741@163.com>
2011-06-09 11:06:32 +0800

    clean the code

--- httptest/test.c
+++ httptest/test.c
@@ -13,7 +13,7 @@
 #include <ctype.h>
 #include <time.h>
 
-#define SET_COOKIE "Set-Cookie:"
+#define SET_COOKIE "set-cookie:"
 #define BLOCK_NUM 1024
 #define COOKIE_NAME_NUM 1024
 #define MONTH_LENGTH 5 
@@ -35,6 +35,7 @@ void  removeTempFile();
 int   checkTime(char expires[]);
 int   deletePath(char cookie[]);
 int   judgeDomainEqual(char path[], char url[]);
+void downloadHtml(char url[]);
 /*********************************************************************
   *filename: httpclient.c
   *purpose: HTTP协议客户端程序，可以用来下载网页
@@ -46,201 +47,12 @@ int   judgeDomainEqual(char path[], char url[]);
 	      *********************************************************************/
 int main(int argc, char *argv[])
 {
-	   int sockfd;
-	   char buffer[1024];
-           struct sockaddr_in server_addr;
-           struct hostent *host;
-           int portnumber,nbytes;
-	   char host_addr[256];
-	   char host_file[1024];
-	   char local_file[256];
-	   FILE * fp;	
-	   char request[1024];	         
-	   int send, totalsend;
-	   int i;
-	   char * pt;
-	   char head_line[1024];
-	   int everyline_number;
-
-
-	   if(argc!=2)
-
-	   {
-
-		   fprintf(stderr,"Usage:%s web-address\a\n",argv[0]);
-
-		   exit(1);
-
-	   }
-
-	   printf("parameter.1 is: %s\n", argv[1]);
-
-	   ToLowerCase(argv[1]);
-	 
-
-
-
-	   GetHost(argv[1], host_addr, host_file, &portnumber);/*分析网址、端口、文件名等*/
-
-	   printf("webhost:%s\n", host_addr);
-
-	   printf("hostfile:%s\n", host_file);
-
-	   printf("portnumber:%d\n\n", portnumber);
-
-
-
-	   if((host=gethostbyname(host_addr))==NULL)/*取得主机IP地址*/
-
-	   {
-
-		   fprintf(stderr,"Gethostname error, %s\n", strerror(errno));
-
-		   exit(1);
-
-	   }
-
-
-
-	   /* 客户程序开始建立 sockfd描述符 */
-
-
-
-	   if((sockfd=socket(AF_INET,SOCK_STREAM,0))==-1)/*建立SOCKET连接*/
-
-	   {
-
-		   fprintf(stderr,"Socket Error:%s\a\n",strerror(errno));
-
-		   exit(1);
-
-	   }
-
-
-
-	   /* 客户程序填充服务端的资料 */
-
-	   bzero(&server_addr,sizeof(server_addr));
-	   server_addr.sin_family=AF_INET;
-	   server_addr.sin_port=htons(portnumber);
-	   server_addr.sin_addr=*((struct in_addr *)host->h_addr);
-
-	   /* 客户程序发起连接请求 */
-
-	   if(connect(sockfd,(struct sockaddr *)(&server_addr),sizeof(struct sockaddr))==-1)/*连接网站*/
-
-	   {
-		   fprintf(stderr,"Connect Error:%s\a\n",strerror(errno));
-		   exit(1);
-	   }
-
-
-		sprintf(request, "GET /%s HTTP/1.1\r\nAccept: */*\r\nAccept-Language: zh-cn\r\n\
-User-Agent: Mozilla/5.0\r\n\
-Host: %s:%d\r\nConnection: Close\r\n\r\n", host_file, host_addr, portnumber); 
-	   printf("%s", request);/*准备request，将要发送给主机*/
-
-	   /*取得真实的文件名*/
-
-	   if(host_file && *host_file)   pt = Rstrchr(host_file, '/');
-	   else pt = 0;
-
-	   memset(local_file, 0, sizeof(local_file));
-
-	   if(pt && *pt)   {
-		   if((pt + 1) && *(pt+1))   strcpy(local_file, pt + 1);
-		   else   memcpy(local_file, host_file, strlen(host_file) - 1);
-	   }
-
-	   else if(host_file && *host_file)   strcpy(local_file, host_file);
-	   else   strcpy(local_file, "index.html");
-	   printf("local filename to write:%s\n\n", local_file);
-
-
-	   /*发送http请求request*/
-
-	   send = 0;totalsend = 0;
-	   nbytes=strlen(request);
-
-	   while(totalsend < nbytes) {
-
-
-
-		   send = write(sockfd, request + totalsend, nbytes - totalsend);
-
-		   if(send==-1)   {printf("send error!%s\n", strerror(errno));exit(0);}
-
-		   totalsend+=send;
-
-		   printf("%d bytes send OK!\n", totalsend);
-
-	   }
-
-
-	   fp = fopen(local_file, "a");
-
-	   if(!fp)   {
-		   printf("create file error! %s\n", strerror(errno));
-		   return 0;
-	   }
-
-	   printf("\nThe following is the response header:\n");
-
-	   i=0;
-	   everyline_number = 0;	
-
-	   createTempFile();
-	   /* 连接成功了，接收http响应，response */
-
-//	   removeTempFile();
-	   while((nbytes=read(sockfd,buffer,1))==1)
-	   {
-
-		   if(i < 4)   {
-			   if(buffer[0] == '\r' || buffer[0] == '\n')  
-			   {
-				   i++;
-				   head_line[everyline_number] = 0;
-				   everyline_number = 0;
-				   printf("%s\n", head_line);
-				   ToLowerCase(head_line);
-				   char *ptr;
-				   if ((ptr = strstr(head_line, SET_COOKIE)) != NULL)
-				   {
-					   fflush(fp);
-					saveCookie(host_addr, ptr + strlen(SET_COOKIE));
-
-
-				   }
-			   }
-			   else 
-			   {
-				   i = 0;
-				   head_line[everyline_number] = buffer[0];
-				   everyline_number ++;
-			   }
-		   }
-		   else 
-		   {
-			   break;
-		   }
-	   }
-
-	   char test_cookie[1024];
-	   getCookie(argv[1], test_cookie);
-	   printf("test_cookie:%s.\n", test_cookie);
-	   while ((nbytes=read(sockfd,buffer,1))==1)	   
-	   {
-		   fwrite(buffer, 1, 1, fp);/*将http主体信息写入文件*/
-		   i++;
-		   if (i % 1024 == 0) fflush(fp);	   
-	   }
-
-	   fclose(fp);
-	   /* 结束通讯 */
-	   close(sockfd);
-
-	   exit(0);
+	if (argc != 2)
+	{
+		printf("error: not enough parameters.\n");
+		return 1;
+	}
+	downloadHtml(argv[1]);
 }
 /********************************************
   功能：搜索字符串右边起的第一个匹配字符
@@ -674,7 +486,11 @@ int getCookie(char url[], char cookie[])
 	if (cookie[0] != 0)
 	{
 		cookie[strlen(cookie) - 1] = 0;
-	
+		return 1;	
+	}
+	else 
+	{
+		return 0;
 	}
 
 }
@@ -722,4 +538,195 @@ int checkTime(char expires[])
 	}
 	return 0;
 }
-/////////////////////////////httpclient.c 结束///////////////////////////////////////////
+void downloadHtml(char url[])
+{
+	   int sockfd;
+	   char buffer[1024];
+           struct sockaddr_in server_addr;
+           struct hostent *host;
+           int portnumber,nbytes;
+	   char host_addr[256];
+	   char host_file[1024];
+	   char local_file[256];
+	   FILE * fp;	
+	   char request[1024];	         
+	   int send, totalsend;
+	   int i;
+	   char * pt;
+	   char head_line[1024];
+	   int everyline_number;
+
+	   ToLowerCase(url);
+
+	   GetHost(url, host_addr, host_file, &portnumber);
+
+//	   printf("webhost:%s\n", host_addr);
+//	   printf("hostfile:%s\n", host_file);
+//	   printf("portnumber:%d\n\n", portnumber);
+
+
+
+	   if((host=gethostbyname(host_addr))==NULL)
+	   {
+
+		   fprintf(stderr,"Gethostname error, %s\n", strerror(errno));
+		   exit(1);
+	   }
+
+
+	   if((sockfd=socket(AF_INET,SOCK_STREAM,0))==-1)
+	   {
+		   fprintf(stderr,"Socket Error:%s\a\n",strerror(errno));
+		   exit(1);
+	   }
+	  
+	   bzero(&server_addr,sizeof(server_addr));
+	   server_addr.sin_family=AF_INET;
+	   server_addr.sin_port=htons(portnumber);
+	   server_addr.sin_addr=*((struct in_addr *)host->h_addr);
+
+	   if(connect(sockfd,(struct sockaddr *)(&server_addr),sizeof(struct sockaddr))==-1)
+	   {
+		   fprintf(stderr,"Connect Error:%s\a\n",strerror(errno));
+		   exit(1);
+	   }
+
+
+	   sprintf(request, "GET /%s HTTP/1.1\r\nAccept: */*\r\nAccept-Language: zh-cn\r\n\
+User-Agent: Mozilla/5.0\r\n\
+Host: %s:%d\r\nConnection: Close\r\n\r\n", host_file, host_addr, portnumber); 
+	   printf("%s", request);
+
+	   
+
+	   if(host_file && *host_file) 
+	   {
+		   pt = Rstrchr(host_file, '/');
+	   }
+	   else 
+	   {
+		   pt = 0;
+	   }
+
+	   memset(local_file, 0, sizeof(local_file));
+
+	   if(pt && *pt)
+	   {
+		   if((pt + 1) && *(pt+1))   
+		   {
+			   strcpy(local_file, pt + 1);
+		   }
+		   else 
+		   {
+			   memcpy(local_file, host_file, strlen(host_file) - 1);
+		   }
+	   }
+
+	   else if(host_file && *host_file) 
+	   {
+		   strcpy(local_file, host_file);
+	   }
+	   else
+	   {
+		   strcpy(local_file, "index.html");
+	   }
+
+	   printf("local filename to write:%s\n\n", local_file);
+
+	   send = 0;
+	   totalsend = 0;
+	   nbytes=strlen(request);
+
+	   while(totalsend < nbytes) {
+
+		   send = write(sockfd, request + totalsend, nbytes - totalsend);
+
+		   if(send==-1)   
+		   {
+			   printf("send error!%s\n", strerror(errno));
+			   exit(0);
+		   }
+
+		   totalsend+=send;
+		   printf("%d bytes send OK!\n", totalsend);
+	   }
+
+	   fp = fopen(local_file, "a");
+
+	   if(!fp)  
+	   {
+		   printf("create file error! %s\n", strerror(errno));
+		   return;
+	   }
+
+	   printf("\nThe following is the response header:\n");
+
+	   i=0;
+	   everyline_number = 0;	
+
+	   createTempFile();
+
+//	   removeTempFile();
+	   while((nbytes=read(sockfd,buffer,1))==1)
+	   {
+
+		   if(i < 4)   {
+			   if(buffer[0] == '\r' || buffer[0] == '\n')  
+			   {
+				   i++;
+				   head_line[everyline_number] = 0;
+				   everyline_number = 0;
+				   printf("%s\n", head_line);
+
+				   ToLowerCase(head_line);
+				   char *ptr;
+				   
+				   printf("HEAD_LINE:%s.\n", head_line);
+
+
+
+				   if ((ptr = strstr(head_line, SET_COOKIE)) != NULL)
+				   {
+					   fflush(fp);
+					   saveCookie(host_addr, ptr + strlen(SET_COOKIE));
+				   }
+			   }
+			   else 
+			   {
+				   i = 0;
+				   head_line[everyline_number] = buffer[0];
+				   everyline_number ++;
+			   }
+		   }
+		   else 
+		   {
+//			   printf("break.\n");
+			   break;
+		   }
+	   }
+
+	   char test_cookie[1024];
+	   if (getCookie(url, test_cookie))
+	   {
+		   printf("test_cookie:%s.\n", test_cookie);
+	   }
+	   else 
+	   {
+	   	pritnf("can not find the cookie in COOKIE.\n");
+	   }
+
+	   while ((nbytes=read(sockfd,buffer,1))==1)	   
+	   {
+		   fwrite(buffer, 1, 1, fp);
+		   i++;
+
+		   if (i % 1024 == 0) 
+		   {
+			   fflush(fp);	   
+		   }
+	   }
+
+	   fclose(fp);
+	   close(sockfd);
+
+}
